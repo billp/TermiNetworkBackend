b7746554c93fc0f9dbb0f7a746dc5c004742dffa
Bill Panagiotopoulos <billp.dev@gmail.com>
2018-04-26 13:25:42 +0300

    Enable static files serve

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TermiNetworkBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.public_file_server.enabled = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
